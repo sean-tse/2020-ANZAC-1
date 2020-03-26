bb002727b3dc1ee66ec182a7b06cae5759a605ef
Sean Tse <seantse3486784401@gmail.com>
2020-03-26 12:48:32 +1100

    test

--- area_rug.py
+++ area_rug.py
@@ -4,8 +4,7 @@ arr = []
 for i in range(0,n):
     row =   input()
     arr.append(row)
-#print(arr)
-#print(arr[0][0])
+
 
 ddict = dict()
 # for every rug placement with left corner at (i,j):
@@ -22,12 +21,11 @@ for i in range(n-s+1):
         else:
             ddict[d_covered] = 1
 
-#print(ddict)
 output = []
 for key in ddict:
     output.append([key, ddict[key]])
 output.sort()
-#print(output)
 
 for i in output:
     print(str(i[0]) + " " + str(i[1]))
+
